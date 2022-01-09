c94ce3378ef8023e589aef5c18a8a82fb8b851a7
JuusoVe <juuso.vesanto@gmail.com>
2022-01-09 10:57:13 +0200

    build: Change project name and run setup.

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='myproject', version='1.0', packages=find_packages())
+setup(
+    name="csgodb",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask",
+    ],
+)
