56c8560b2b12a1ad3a609ac9bdf9c36b4bc2cc6c
JuusoVe <juuso.vesanto@gmail.com>
2022-04-10 12:03:20 +0300

    build: Added config file.

--- api/src/config.py
+++ api/src/config.py
@@ -1,7 +1,10 @@
 from os import getenv
 
 
-class FlaskConfig(object):
+class FlaskConfig:
+    """
+    Shared config for all environments."""
+
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
@@ -11,13 +14,25 @@ class FlaskConfig(object):
 
 
 class FlaskProductionConfig(FlaskConfig):
+    """
+    Production config,
+    """
+
     DEBUG = False
 
 
 class FlaskDevelopmentConfig(FlaskConfig):
+    """
+    Development config,
+    """
+
     DEVELOPMENT = True
     DEBUG = True
 
 
 class FlaskTestingConfig(FlaskConfig):
+    """
+    Testing config,
+    """
+
     TESTING = True
