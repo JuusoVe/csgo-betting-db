345e25889222c9e0f506cffe0a45c57087d9cbe1
JuusoVe <juuso.vesanto@gmail.com>
2022-08-14 17:14:58 +0300

    feat: Added security groups.

--- infra/modules/networking/variables.tf
+++ infra/modules/networking/variables.tf
@@ -12,3 +12,8 @@ variable "public_subnets" {
   description = "a list of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
   default     = ["10.0.16.0/20", "10.0.48.0/20", "10.0.80.0/20"]
 }
+
+variable "ecs_container_port" {
+  description = "Port the"
+  default = 80
+}
