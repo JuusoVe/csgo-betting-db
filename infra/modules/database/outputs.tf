48ad512fa100df34cddc9b89e62143180428d43f
JuusoVe <juuso.vesanto@gmail.com>
2022-08-19 18:29:08 +0300

    test: Pipeline outputs.

--- infra/modules/database/outputs.tf
+++ infra/modules/database/outputs.tf
@@ -1,17 +1,20 @@
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.csgo-betting-db.address
-  sensitive   = true
-}
-
-output "rds_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.csgo-betting-db.port
-  sensitive   = true
-}
-
 output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.csgo-betting-db.username
   sensitive   = true
+}
+
+output "rds_passwords" {
+    description = "RDS instance root password"
+    sensitive   = true
+}
+
+output "test_output" {
+    description = "Dev time test."
+    value = "thisBESTtest"
+}
+
+output "rds_connection_string" {
+    descriptions = "Full connection string."
+    value = "postgresql://${aws_db_instance.csgo-betting-db.username}:${var.db_password}@${aws_db_instance.csgo-betting-db.endpoint}"
 }
