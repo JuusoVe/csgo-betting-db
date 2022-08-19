e253fbd092f58479b25871b1d92dc90c2855def6
JuusoVe <juuso.vesanto@gmail.com>
2022-08-19 18:34:25 +0300

    fix: Syntax and typo.

--- infra/modules/database/outputs.tf
+++ infra/modules/database/outputs.tf
@@ -1,20 +1,9 @@
-output "rds_username" {
-  description = "RDS instance root username"
-  value       = aws_db_instance.csgo-betting-db.username
-  sensitive   = true
-}
-
-output "rds_passwords" {
-    description = "RDS instance root password"
-    sensitive   = true
-}
-
 output "test_output" {
-    description = "Dev time test."
-    value = "thisBESTtest"
+  description = "Dev time test."
+  value       = "thisBESTtest"
 }
 
 output "rds_connection_string" {
-    descriptions = "Full connection string."
-    value = "postgresql://${aws_db_instance.csgo-betting-db.username}:${var.db_password}@${aws_db_instance.csgo-betting-db.endpoint}"
+  description = "Full connection string."
+  value       = "postgresql://${aws_db_instance.csgo-betting-db.username}:${var.db_password}@${aws_db_instance.csgo-betting-db.endpoint}"
 }
