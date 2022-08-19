24d7f8f80ed6a4be0ebcd98ee53395412facba46
JuusoVe <juuso.vesanto@gmail.com>
2022-08-19 13:56:13 +0300

    fix: Output.

--- infra/modules/networking/outputs.tf
+++ infra/modules/networking/outputs.tf
@@ -10,7 +10,7 @@ output "subnet_group_name" {
 
 output "aws_alb_target_group_arn" {
   description = "Identifier of the loadbalancer target group."
-  value       = aws_alb_target_group.cbdb-target-group.arn
+  value       = aws_alb_target_group.main.arn
 }
 
 output "ecs_service_security_groups" {
