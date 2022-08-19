3d4da9ee3933c5f276b2656e96d205529e129267
JuusoVe <juuso.vesanto@gmail.com>
2022-08-19 15:19:10 +0300

    fix: Task execution role.

--- infra/modules/container-service/main.tf
+++ infra/modules/container-service/main.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_task_definition" "cbdb-api" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
   memory                   = 512
-  execution_role_arn       = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([{
     name        = "cbdb-api-container"
