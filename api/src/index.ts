6fda772bef722216867e2b791abd69ab9716ea6b
JuusoVe <juuso.vesanto@gmail.com>
2022-08-28 10:21:08 +0300

    build: Added typescript.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { Request, Response } from 'express';
+
 const app = express();
 const port = 5000;
 
-app.get('/', (_req, res) => {
+app.get('/', (_req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
